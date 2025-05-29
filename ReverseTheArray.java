12d1bdf2155af2c7c40c16104160d52312ecca9b
Borish1 <borishningombam@gmail.com>
2025-05-29 23:44:34 +0530

    Reverse of an Array

---
+++ ReverseTheArray.java
@@ -0,0 +1,10 @@
+package Java_code_from_zero_to_master;
+
+public class ReverseTheArray {
+    public static void main(String[] args){
+        int[] arr = {12,43,65,76,23,65};
+        for(int i = arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
