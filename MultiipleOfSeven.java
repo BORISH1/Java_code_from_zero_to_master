9f0c247185e316191abeedb05acc97edc9715857
Borish1 <borishningombam@gmail.com>
2025-05-31 23:46:20 +0530

    multiple of 7

---
+++ MultiipleOfSeven.java
@@ -0,0 +1,9 @@
+package Java_code_from_zero_to_master;
+
+public class MultiipleOfSeven {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println("7 * "+ i + " = " + 7*i);
+        }
+    }
+}
