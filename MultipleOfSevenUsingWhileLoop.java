023a27a77997a15922df69ded2b3883bea9b663e
Borish1 <borishningombam@gmail.com>
2025-05-31 23:50:05 +0530

    multiple of 7 using while loop

---
+++ MultipleOfSevenUsingWhileLoop.java
@@ -0,0 +1,12 @@
+package Java_code_from_zero_to_master;
+
+public class MultipleOfSevenUsingWhileLoop {
+    public static void main(String[] args){
+        int i = 1;
+        while(i<=10){
+            System.out.println(" 7 * "+i+" = " + i*7);
+            i++;
+        }
+    }
+
+}
