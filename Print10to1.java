9fe0e56559e72e28ff62c43edffecdc9bbf44e4a
Borish1 <borishningombam@gmail.com>
2025-06-01 23:54:32 +0530

    print 10 to 1

---
+++ Print10to1.java
@@ -0,0 +1,9 @@
+package Java_code_from_zero_to_master;
+
+public class Print10to1 {
+    public static void main(String[] args){
+        for(int i=10;i>= 0;i--){
+            System.out.println(i);
+        }
+    }
+}
