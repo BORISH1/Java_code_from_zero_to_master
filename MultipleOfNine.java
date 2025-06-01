f709b033f9e480d34f841144865684d050df032b
Borish1 <borishningombam@gmail.com>
2025-06-02 00:02:16 +0530

    multiple of 9

---
+++ MultipleOfNine.java
@@ -0,0 +1,10 @@
+package Java_code_from_zero_to_master;
+
+public class MultipleOfNine {
+    public static void main(String[] args){
+        for(int i = 1;i<=10;i++){
+            System.out.println(" 9 * "+ i + " = " + i*9);
+        }
+
+    }
+}
