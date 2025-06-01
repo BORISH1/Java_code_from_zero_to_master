b1fa807c6d645bc2b43348567796f0a734b0a791
Borish1 <borishningombam@gmail.com>
2025-06-01 09:57:39 +0530

    star pattern

---
+++ PrintTrianglePattern.java
@@ -0,0 +1,13 @@
+package Java_code_from_zero_to_master;
+
+public class PrintTrianglePattern {
+    public static void main(String[] args){
+        for ( int i =0 ;i<=4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
