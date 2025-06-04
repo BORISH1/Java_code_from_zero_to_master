76d2d5e60ac0e861afd9b4c07947d6766ffeba07
Borish1 <borishningombam@gmail.com>
2025-06-04 15:08:30 +0530

    code

--- addTwoNumberUsingMethod.java
+++ addTwoNumberUsingMethod.java
@@ -2,7 +2,7 @@ package Java_code_from_zero_to_master;
 
 public class addTwoNumberUsingMethod {
     public int add(int a,int b){
-        return a*b;
+        return a+b;
     }
 
     public static void main(String[] args) {
