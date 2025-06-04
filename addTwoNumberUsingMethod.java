3d1f6e6f4d5f1e6ff8c815e7b5b75339e3bc2a11
Borish1 <borishningombam@gmail.com>
2025-06-04 15:03:35 +0530

    code

---
+++ addTwoNumberUsingMethod.java
@@ -0,0 +1,13 @@
+package Java_code_from_zero_to_master;
+
+public class addTwoNumberUsingMethod {
+    public int add(int a,int b){
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        addTwoNumberUsingMethod obj = new addTwoNumberUsingMethod();
+        int addition = obj.add(23,32);
+        System.out.println("The sum is " + addition);
+    }
+}
