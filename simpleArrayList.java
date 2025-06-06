e12ecbef99ac16ca64efd2bffeed3b41313cf142
Borish1 <borishningombam@gmail.com>
2025-06-06 09:55:05 +0530

    Arraylist

---
+++ simpleArrayList.java
@@ -0,0 +1,17 @@
+package Java_code_from_zero_to_master;
+
+import java.util.ArrayList;
+
+public class simpleArrayList {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(20);
+        al.add(40);
+        al.add(23.43f);
+        al.add('D');
+        al.add("Alex");
+        System.out.println(al);
+    }
+
+
+}
