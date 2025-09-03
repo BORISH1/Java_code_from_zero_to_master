66bd680964d9f3aee329eea4c3dca1798668b664
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-09-04 00:13:34 +0530

    updated

---
+++ CheckingGrade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CheckingGrade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Mark: ");
+        int mark = sc.nextInt();
+        if(mark >=90){
+            System.out.println("O grade");
+        } else if (mark>=80) {
+            System.out.println("A+ grade");
+        } else if (mark>=70) {
+            System.out.println("A grade");
+        } else if (mark>=60) {
+            System.out.println("B+ grade");
+        } else if (mark>=50) {
+            System.out.println("B grade");
+        } else if (mark>=55) {
+            System.out.println("C grade");
+        }
+        else {
+            System.out.println("Arrear");
+        }
+    }
+}
