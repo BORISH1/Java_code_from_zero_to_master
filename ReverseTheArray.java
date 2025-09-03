07b0df7025eea5ba2485a9371e7c045d47a43c4a
Ningombam Borish Singh <131790729+BORISH1@users.noreply.github.com>
2025-09-03 22:31:42 +0530

    updated

--- ReverseTheArray.java
+++ ReverseTheArray.java
@@ -4,7 +4,7 @@ public class ReverseTheArray {
     public static void main(String[] args){
         int[] arr = {12,43,65,76,23,65};
         for(int i = arr.length-1;i>=0;i--){
-            System.out.println(arr[i]);
+            System.out.print( " "+arr[i]);
         }
     }
 }
